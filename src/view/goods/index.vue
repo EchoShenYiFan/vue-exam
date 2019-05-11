18680da6454ac2f0e839e12acaadeeaa8e5f5b20
tangmaomao <tangtang5963@163.com>
2019-05-12 00:29:21 +0800

    aaa

--- src/view/goods/index.vue
+++ src/view/goods/index.vue
@@ -12,7 +12,7 @@
         <div class="goods-price">{{ formatPrice(goods.price) }}</div>
       </van-cell>
       <van-cell class="goods-express">
-        <van-col span="10">运费：{{ goods.express }}</van-col>
+        <van-col span="10">运费的：{{ goods.express }}</van-col>
         <van-col span="14">剩余：{{ goods.remain }}</van-col>
       </van-cell>
     </van-cell-group>
